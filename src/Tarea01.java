0209b493743d1456cda0489a4ab90f514a3cf39d
migueldj80 <migueldjirc@gmail.com>
2022-08-02 14:01:20 +0200

    Segundo

--- src/Tarea01.java
+++ src/Tarea01.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Tarea01 {
+/**
+ * Tarea 1 Miguel Ledesma Palacios
+ * OpenBootCamp
+ */
+public class Tarea01 {
+
+    public static void main(String[] args) {
+        System.out.println("Sumamos los valores 5,10 y 24");
+        System.out.println("El resultado es : " + suma(5, 10, 24));
+    }
+
+    public static int suma(int a, int b, int c) {
+
+        return a + b + c;
+    }
+
 }
